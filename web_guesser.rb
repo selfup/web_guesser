7e3587cd9fe0968a010614f5b2173ef981fa469b
Regis <boudinot.regis@yahoo.com>
2015-08-17 15:20:23 -0600

    Put logic in the controller
    Extension is next

--- web_guesser.rb
+++ web_guesser.rb
@@ -3,6 +3,32 @@ require 'sinatra/reloader'
 
 SECRET_NUMBER = rand(100)
 
+def number
+	SECRET_NUMBER
+end
+
+def check_guess(guess)
+	guess = guess.to_i
+	too_high = number + 5
+	too_low = number - 5
+	if guess == number
+		"You got it right!
+		The SECRET NUMBER is: <strong>#{number}</strong>"
+	elsif guess >= too_high
+		"Way too high!"
+	elsif guess <= too_low
+		"Way too low!"
+	elsif guess > number
+		"Too high!"
+	elsif guess < number
+		"Too low!"
+	else
+		""
+	end
+end
+
 get '/' do
-	erb :index, :locals => {:number => SECRET_NUMBER}
+	guess = params["guess"]
+	message = check_guess(guess)
+	erb :index, :locals => {:number => SECRET_NUMBER, :message => message}
 end
