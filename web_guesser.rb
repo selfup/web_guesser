a1c33bb48baa1965a7f3e670dff2050a1366b43d
Regis <boudinot.regis@yahoo.com>
2017-05-03 07:51:42 -0600

    all hooked up

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,53 +1,12 @@
+require 'json'
 require 'sinatra'
+require "sinatra/json"
 require 'sinatra/reloader'
 
-SECRET_NUMBER = rand(100)
-
-def number
-	SECRET_NUMBER
-end
-
-def check_guess(guess)
-	guess = guess.to_i unless guess.nil?
-	too_high = number + 5
-	too_low = number - 5
-	if guess.nil?
-		"Take a guess!"
-	elsif
-		guess == number
-		"You got it right!
-		The SECRET NUMBER is: <strong>#{number}</strong>"
-	elsif guess >= too_high
-		"Way too high!"
-	elsif guess <= too_low
-		"Way too low!"
-	elsif guess > number
-		"Too high!"
-	elsif guess < number
-		"Too low!"
-	else
-		""
-	end
-end
-
-def color_guess
-	if @@message == "Way too low!" || @@message == "Way too high!"
-		"background-color:#F0501A"
-	elsif @@message == "Too low!" || @@message == "Too high!"
-		"background-color:#D4A190"
-	elsif @@message.start_with?("You") 
-	  "background-color:#A1D490"
-	else
-		"background-color:#90C3D4"
-	end
-end
-
 get '/' do
-	guess = params["guess"]
-	@@message = check_guess(guess)
-	erb :index, :locals => {
-			:number => SECRET_NUMBER,
-		  :message => @@message,
-		  :color_guess => color_guess
-		}
+	erb :index
+end
+
+get '/api/v1/some_data.json' do
+  json({ :key1 => 'value1', :key2 => 'value2' })
 end
